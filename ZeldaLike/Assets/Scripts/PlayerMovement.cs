9104d00a879f1aabb1867ed9340f77a7c0b458aa
Hashir-Qureshi <qureshihashir@yahoo.com>
2018-12-17 18:40:39 -0500

    init commit

---
+++ ZeldaLike/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerState
+{
+    walk,
+    attack,
+    interact
+}
+
+
+
+public class PlayerMovement : MonoBehaviour {
+    public PlayerState currentState;
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private Animator animator;
+	// Use this for initialization
+	void Start () {
+        currentState = PlayerState.walk;
+        animator = GetComponent<Animator>();
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("attack") && currentState != PlayerState.attack)
+        {
+            StartCoroutine(AttackCo());
+        }else if(currentState == PlayerState.walk)
+        {
+            UpdateAnimationAndMove();
+        }
+
+        
+    }
+
+    private IEnumerator AttackCo()
+    {
+        animator.SetBool("attacking",true);
+        currentState = PlayerState.attack;
+        yield return null;
+        animator.SetBool("attacking", false);
+        yield return new WaitForSeconds(.33f);
+        currentState = PlayerState.walk;
+    }
+
+
+    void UpdateAnimationAndMove()
+    {
+        if (change != Vector3.zero)
+        {
+            change.Normalize();
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+        
+    }
+
+    void MoveCharacter()
+    {
+        //myRigidBody.velocity = change * speed;
+        myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
+    }
+
+}
